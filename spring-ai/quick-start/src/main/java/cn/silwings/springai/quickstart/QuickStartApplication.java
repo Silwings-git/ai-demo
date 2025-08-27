108dbe1e4148e285fb47cdffa7eff99789dda84d
Silwings <1910506332@qq.com>
2025-08-27 19:58:49 +0800

    ChatMemory

--- spring-ai/quick-start/src/main/java/cn/silwings/springai/quickstart/QuickStartApplication.java
+++ spring-ai/quick-start/src/main/java/cn/silwings/springai/quickstart/QuickStartApplication.java
@@ -1,4 +1,4 @@
-package cn.silwings.springai.quickstart.config;
+package cn.silwings.springai.quickstart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
