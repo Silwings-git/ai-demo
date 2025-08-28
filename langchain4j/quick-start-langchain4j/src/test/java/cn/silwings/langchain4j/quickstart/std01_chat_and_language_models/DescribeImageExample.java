16689c3d9e425d39b0658f2cd249278fa639c69a
silwings <silwings@163.com>
2025-08-29 00:35:10 +0800

    chat memory

---
+++ langchain4j/quick-start-langchain4j/src/test/java/cn/silwings/langchain4j/quickstart/std01_chat_and_language_models/DescribeImageExample.java
@@ -0,0 +1,28 @@
+package cn.silwings.langchain4j.quickstart.std01_chat_and_language_models;
+
+import cn.silwings.aidemo.common.utils.Env;
+import cn.silwings.aidemo.common.utils.Model;
+import dev.langchain4j.community.model.dashscope.QwenChatModel;
+import dev.langchain4j.data.image.Image;
+import dev.langchain4j.data.message.ImageContent;
+import dev.langchain4j.data.message.TextContent;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.chat.response.ChatResponse;
+
+public class DescribeImageExample {
+    public static void main(String[] args) {
+        final ChatModel model = QwenChatModel.builder()
+                .apiKey(Env.ALI_AI_KEY.get())
+                .modelName(Model.QWEN.QWEN_VL_PLUS)
+                .build();
+
+        final UserMessage userMessage = UserMessage.from(
+                TextContent.from("描述一下这张图片"),
+                ImageContent.from(Image.builder().url("https://pic.rmb.bdstatic.com/bjh/bb8f413b5a1/241021/c5f406065fab31b238078065106ed894.jpeg").build())
+        );
+
+        final ChatResponse response = model.chat(userMessage);
+        System.out.println(response.aiMessage().text());
+    }
+}
