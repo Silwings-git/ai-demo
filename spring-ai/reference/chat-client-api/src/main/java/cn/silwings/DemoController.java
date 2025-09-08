7ffc46b4b787cd9af3634c449f70cebde890d338
Silwings <1910506332@qq.com>
2025-09-08 22:27:19 +0800

    spring ai

--- spring-ai/reference/chat-client-api/src/main/java/cn/silwings/DemoController.java
+++ spring-ai/reference/chat-client-api/src/main/java/cn/silwings/DemoController.java
@@ -4,6 +4,7 @@ import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatModel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     private final DashScopeChatModel model;
+
+    private final OpenAiChatModel openAiChatModel;
 
     @GetMapping("/demo")
     public User demo() {
@@ -26,5 +29,20 @@ public class DemoController {
         System.out.println("content = " + content);
         return content;
     }
+
+    @GetMapping("/demo2")
+    public User demo2() {
+
+        final ChatClient chatClient = ChatClient.builder(this.openAiChatModel)
+                .defaultAdvisors(new SimpleLoggerAdvisor())
+                .build();
+        final User content = chatClient.prompt()
+                .user("提取用户信息: hello,我叫Silwings,今年18岁,请多关照!")
+                .call()
+                .entity(User.class);
+        System.out.println("content = " + content);
+        return content;
+    }
+
 
 }
